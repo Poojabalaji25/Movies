d98394fd56ef78a174b7c1afedb65732a5dfa529
Poojabalaji25 <65073813+Poojabalaji25@users.noreply.github.com>
2021-09-01 22:27:18 +0530

    Add files via upload

---
+++ src/main/java/movie/Movie.java
@@ -0,0 +1,51 @@
+package movie;
+
+public class Movie {
+	
+	private String movieName;
+	private String actor;
+	private String actress;
+	private String director;
+	private int releaseYear;
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public String getActor() {
+		return actor;
+	}
+
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+
+	public String getActress() {
+		return actress;
+	}
+
+	public void setActress(String actress) {
+		this.actress = actress;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+}
